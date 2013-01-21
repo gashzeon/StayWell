d9265dbbe504298d030240f0c1227b9f6e68fe51
Gash <shawnpoke@hotmail.com>
2013-01-21 10:33:26 +0800

    Updated CarCollection

--- StayWell/src/staywell/ui/user/carcollection/CarCollection.java
+++ StayWell/src/staywell/ui/user/carcollection/CarCollection.java
@@ -1,105 +1,93 @@
 package staywell.ui.user.carcollection;
 
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.Timer;
+
+
 import java.awt.Font;
-import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.sql.Time;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.swing.GroupLayout;
-import javax.swing.ImageIcon;
+import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 import javax.swing.Timer;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.border.EmptyBorder;
 
-import staywell.entities.Session;
+
 import staywell.ui.Layout;
 import staywell.ui.MainFrame;
+import staywell.ui.user.Homepage;
+
+
 public class CarCollection extends Layout {
 	private JTextField textField;
 
 	/**
 	 * Create the panel.
 	 */
-	MainFrame f;
-	
-	public CarCollection(MainFrame frame) {
-		f = frame;
-		
-		Session session = new Session();
-		if(f.getSession() != null){
-			session = f.getSession();
-		}
-		points = session.getPoints();
-		
-		setBounds(new Rectangle(0, 0, 1024, 768));
-		setLayout(null);
-		
-		final JLabel Time = new JLabel("New label");
-		Time.setFont(new Font("Candara", Font.BOLD, 20));
-		add(Time);
+	public CarCollection() {
 
-		final DateFormat timeFormat = new SimpleDateFormat("EEE, d MMM yyyy H:mm:ss z");
-		ActionListener timerListener = new ActionListener()
-		{
-		public void actionPerformed(ActionEvent e)
-		{
-		Date date = new Date();
-		String time = timeFormat.format(date);
-		Time.setText(time);
-		}
-		};
-		Timer timer = new Timer(1000, timerListener);
-		
+		setBorder(new EmptyBorder(5,5,5,5));
+		setBounds(0,0,1024,768);
+		setLayout(null);
 		// to make sure it doesn't wait one second at the start
-		timer.setInitialDelay(0);
-		timer.start();
 		
 		
 		JButton btnNewButton = new JButton("Submit");
 		btnNewButton.setFont(new Font("Candara", Font.BOLD, 20));
-		add(btnNewButton, "3, 10, fill, fill");
+		btnNewButton.setBounds(434, 410, 150, 36);
+		add(btnNewButton);
 		
 		JLabel Title = new JLabel("Car Collection");
 		Title.setFont(new Font("Candara", Font.BOLD, 30));
+		Title.setBounds(0, 47, 1024, 86);
 		Title.setHorizontalAlignment(SwingConstants.CENTER);
-		add(Title, "1, 2, 5, 3, fill, fill");
+		add(Title);
 		
 		JLabel lblNewLabel = new JLabel("Please proceed to the hotel lobby for collection");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Candara", Font.BOLD, 20));
-		add(lblNewLabel, "1, 12, 5, 1, center, top");
+		lblNewLabel.setBounds(256, 473, 514, 52);
+		add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Please enter your car plate number");
 		lblNewLabel_1.setFont(new Font("Candara", Font.BOLD, 20));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		add(lblNewLabel_1, "1, 6, 5, 1, center, fill");
+		lblNewLabel_1.setBounds(325, 207, 381, 36);
+		add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Enter here:");
 		lblNewLabel_2.setFont(new Font("Candara", Font.BOLD, 20));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		add(lblNewLabel_2, "1, 8, right, fill");
+		lblNewLabel_2.setBounds(301, 314, 100, 36);
+		add(lblNewLabel_2);
+		
+		
 		
 		textField = new JTextField();
-		add(textField, "3, 8, 3, 1, left, fill");
+		textField.setBounds(411, 315, 221, 36);
+		add(textField);
 		textField.setColumns(10);
 		
 		JLabel InnerBackground = new JLabel("");
+		InnerBackground.setBounds(112, 123, 803, 465);
 		InnerBackground.setIcon(new ImageIcon(CarCollection.class.getResource("/staywell/image/PanelBackground.png")));
 		InnerBackground.setHorizontalAlignment(SwingConstants.CENTER);
-		add(InnerBackground, "1, 4, 5, 9, center, fill");
-		
-		// Set a larger, nicer font for the digital clock than the default.
-		Time.setFont(new Font("CANDARA", Font.BOLD, 20));
-		this.add(Time);
+		add(InnerBackground);
 		this.setVisible(true);
 		
 		super.setLayout();
