7ee75d5f0d6f0d706df7584d044f83f2651e97f2
Long <Long@Jiajun-Laptop>
2013-01-22 20:05:24 +0800

    Cleaning Services Done

--- StayWell/src/staywell/ui/user/CleaningServices.java
+++ StayWell/src/staywell/ui/user/CleaningServices.java
@@ -1,10 +1,14 @@
 package staywell.ui.user;
+
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Frame;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -18,8 +22,12 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.border.LineBorder;
 
+import staywell.entities.FeedbackEntities;
+import staywell.entities.dao.FeedbackDAO;
 import staywell.ui.Layout;
 import staywell.ui.MainFrame;
+import javax.swing.JRadioButton;
+import java.awt.Component;
 
 public class CleaningServices extends Layout {
 
@@ -35,67 +43,16 @@ public class CleaningServices extends Layout {
 		setBounds(new Rectangle(0, 0, 1024, 768));
 		setLayout(null);
 
-		String columnNames[] = { "TIME","SELECT"};
-		String dataValues[][] = { { "TIME", "SELECT (To select key in 1 next to the time slot)"},
-				{ "9.00am - 11.00am", ""},
-				{ "11.00am-1.00pm", ""},
-				{ "1.00pm - 3.00pm", ""},
-				{ "3.00pm - 5.00pm", ""}};
-		TableModel model = new DefaultTableModel(dataValues, columnNames) {
-			public boolean isCellEditable(int row, int column) {
-				switch (column) {
-				case 1:
-					switch (row) {
-					case 1:
-					case 2:
-					case 3:
-					case 4:
-						return true;
-						
-					default: return false;
-					}
-				default:
-					return false;
-				}
-			}
-
-		};
 		
-
-		JTable table = new JTable(model);
-		table.setBorder(new LineBorder(new Color(0, 0, 0)));
-		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-		centerRenderer.setHorizontalAlignment(JLabel.CENTER);
-		table.getColumnModel().getColumn(0).setCellRenderer(centerRenderer);
-		table.getColumnModel().getColumn(1).setCellRenderer(centerRenderer);
-		table.getColumnModel().getColumn(0).setMaxWidth(1000);
-		table.getColumnModel().getColumn(0).setMinWidth(150);
-		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		table.setRowHeight(50);
-		table.setFont(new Font("Candara", Font.BOLD, 20));
-		table.setBounds(169, 250, 685, 250);
-		add(table);
 		
-		JButton cashing = new JButton("");
-		cashing.setContentAreaFilled(false);
-		cashing.setBorderPainted(false);
-		cashing.setIcon(new ImageIcon(Homepage.class.getResource("/staywell/image/Cashing70X70.png")));
-		cashing.setBorder(null);
-		cashing.setBounds(477, 511, 70, 70);
-		add(cashing);
 		
-		JLabel orderLb = new JLabel("Order!");
-		orderLb.setHorizontalAlignment(SwingConstants.CENTER);
-		orderLb.setForeground(Color.RED);
-		orderLb.setFont(new Font("Candara", Font.BOLD, 20));
-		orderLb.setBounds(482, 592, 60, 28);
-		add(orderLb);
 		
-		JLabel lblCleaningServices = new JLabel("Cleaning Services");
+		
+		
+		JLabel lblCleaningServices = new JLabel("Housekeeping Services");
 		lblCleaningServices.setForeground(Color.RED);
 		lblCleaningServices.setFont(new Font("Candara", Font.BOLD, 30));
-		lblCleaningServices.setBounds(395, 11, 233, 30);
+		lblCleaningServices.setBounds(395, 11, 319, 30);
 		add(lblCleaningServices);
 		
 		JButton logo = new JButton();
@@ -116,6 +73,67 @@ public class CleaningServices extends Layout {
 		});
 		add(logo);
 		
+		JLabel lblRoomCS = new JLabel("The following is the time slot for the next day housekeeping. Please selected ur prefered timeslot for your housekeeping to take place and click on submit. ");
+		lblRoomCS.setFont(new Font("Candara", Font.BOLD, 16));
+		lblRoomCS.setBounds(43, 91, 755, 14);
+		add(lblRoomCS);
+		
+		JRadioButton rdbtn0800 = new JRadioButton("0800");
+		rdbtn0800.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtn0800.setBounds(206, 190, 258, 36);
+		add(rdbtn0800);
+		
+		JRadioButton rdbtn0900 = new JRadioButton("0900");
+		rdbtn0900.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtn0900.setBounds(206, 243, 258, 36);
+		add(rdbtn0900);
+		
+		JRadioButton rdbtn1000 = new JRadioButton("1000");
+		rdbtn1000.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtn1000.setBounds(206, 302, 258, 36);
+		add(rdbtn1000);
+		
+		JRadioButton rdbtn1100 = new JRadioButton("1100");
+		rdbtn1100.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtn1100.setBounds(206, 357, 258, 36);
+		add(rdbtn1100);
+		
+		JRadioButton rdbtn1200 = new JRadioButton("1200");
+		rdbtn1200.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtn1200.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		rdbtn1200.setBounds(206, 412, 258, 36);
+		add(rdbtn1200);
+		
+		JRadioButton rdbtn1300 = new JRadioButton("1300");
+		rdbtn1300.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtn1300.setBounds(568, 190, 258, 36);
+		add(rdbtn1300);
+		
+		JRadioButton rdbtn1400 = new JRadioButton("1400");
+		rdbtn1400.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtn1400.setBounds(568, 243, 258, 36);
+		add(rdbtn1400);
+		
+		JRadioButton rdbtn1500 = new JRadioButton("1500");
+		rdbtn1500.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtn1500.setBounds(568, 302, 258, 36);
+		add(rdbtn1500);
+		
+		JRadioButton rdbtn1600 = new JRadioButton("1600");
+		rdbtn1600.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtn1600.setBounds(568, 357, 258, 36);
+		add(rdbtn1600);
+		
+		JRadioButton rdbtn1700 = new JRadioButton("1700");
+		rdbtn1700.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtn1700.setBounds(568, 412, 258, 36);
+		add(rdbtn1700);
+		
 		super.setLayout();
+		
+		/// CONFIRM START HERE
+		
+		///CONFIRM END HERE
 	}
 }
+
